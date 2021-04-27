1d650d712e780c3d5dd8626e500d7bb9e1244705
anvar <anvar5710@mail.ru>
2021-04-28 00:00:11 +0300

    with telegram

--- tikfake/app.py
+++ tikfake/app.py
@@ -1,13 +1,29 @@
-import pathlib
+import moviepy.editor as mpe
 
-from tikfake.animation_pipeline import AnimationPipeline
-
-
-def main():
-    animation_pipeline = AnimationPipeline()
-    animation_pipeline.process(pathlib.Path("/Users/klochkovanton/Downloads/be68149df83e44fdbea64b9a0ca323ac.mp4"),
-                               pathlib.Path("output.mp4"))
+from TikTokApi import TikTokApi
 
 
-if __name__ == '__main__':
-    main()
+def downloader_video_with_audio(video_with_audio, video_without_audio):
+    """Function for getting video"""
+    audio = get_audio(video_with_audio)
+    my_clip = get_video_with_audio(video_without_audio, audio)
+    my_clip.write_videofile("movie.mp4")
+
+
+def get_audio(video):
+    """Function for getting audio from video"""
+    videoclip = mpe.VideoFileClip(video)
+    return videoclip.audio
+
+
+def get_video_with_audio(video_without_audio, audio):
+    """Function for getting audio from video"""
+    video_clip = mpe.VideoFileClip(video_without_audio)
+    return video_clip.set_audio(audio)
+
+
+def downloader_video_from_link(url):
+    """Download the video from the link"""
+    video = TikTokApi().get_video_by_url(url)
+    with open("video.mp4", "wb") as out:
+         out.write(video)
---
+++ tikfake/bot.py
@@ -0,0 +1,57 @@
+import logging
+import pathlib
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+from tikfake.animation_pipeline import AnimationPipeline
+from tikfake.app import downloader_video_with_audio, downloader_video_from_link
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def link(update, context):
+    """Process entered URL."""
+    url = update.message.text
+    update.message.reply_text(f'TikTok url: {url}')
+
+    # download video in "video.mp4"
+    downloader_video_from_link(url)
+
+    # ml
+    animation_pipeline = AnimationPipeline()
+    animation_pipeline.process(pathlib.Path("video.mp4"), pathlib.Path("output.mp4"))
+
+    # download in "movie.mp4"
+    downloader_video_with_audio("video.mp4", "output.mp4")
+
+
+def help(update, context):
+    """Display help."""
+    update.message.reply_text('Enter a TikTok URL and get an animated video generated from it.')
+
+
+def error(update, context):
+    """Log errors caused by updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main(token):
+    """Start the bot."""
+
+    updater = Updater(token, use_context=True)
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('help', help))
+    dp.add_handler(MessageHandler(Filters.text, link))
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    print('Enter TikFake Telegram bot token:')
+    token = input()
+    main(token)
